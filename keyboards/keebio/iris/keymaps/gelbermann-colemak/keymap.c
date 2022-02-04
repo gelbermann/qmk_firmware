d1aa639d779892a5741dd43dd1602c6f75a737f1
Niv Gelbermann <niv.gelbermann@gmail.com>
2022-02-04 22:45:43 +0200

    improving code quality

--- keyboards/keebio/iris/keymaps/gelbermann-colemak/keymap.c
+++ keyboards/keebio/iris/keymaps/gelbermann-colemak/keymap.c
@@ -1,7 +1,7 @@
 #include QMK_KEYBOARD_H
 
 // Layers
-#define COLEM     DF(_COLEMAK)
+#define COLEM     DF(_COLEMAK_DH)
 #define QWERT     DF(_QWERTY)
 #define RAISE     MO(_RAISE)
 #define LOWER     MO(_LOWER)
@@ -11,7 +11,7 @@
 #define KC_UNDS   LSFT(KC_MINS)  // _
 
 enum layers_keycodes {
-  _COLEMAK,
+  _COLEMAK_DH,
   _QWERTY,
   _LOWER,
   _RAISE,
@@ -19,24 +19,24 @@ enum layers_keycodes {
 };
 
 enum macros_keycodes {
-  _SW_LANG,
-  _SHFT_L,
-  _SHFT_R,
-  _A_SHFT_L,
-  _A_SHFT_R,
+  SW_LANG = SAFE_RANGE,
+  LALT_L,
+  LALT_R,
+  A_SHFT_L,
+  A_SHFT_R,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  [_COLEMAK] = LAYOUT(
+  [_COLEMAK_DH] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F12,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,                               KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_QUOT,
+     KC_TAB,  KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                               KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_QUOT,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LSFT, KC_A,    KC_R,    KC_S,    KC_T,    KC_D,                               KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_RSFT,
+     KC_LSFT, KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                               KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    KC_RSFT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_ENT,           _______, KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSPC,
+     KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,    KC_ENT,           _______, KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSPC,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     KC_LGUI, LOWER,   KC_ENT,                    KC_SPC,  RAISE,   KC_RALT
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
@@ -72,15 +72,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_RAISE] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
-     _______, _______, _______, _______, _______, COLEM,                              QWERT,   _______, _______, _______, _______, _______,
+     _______, _______, _______, _______, _______, COLEM,                              QWERT,   KC_NUM,  _______, _______, _______, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_CAPS, _______, KC_AMPR, KC_ASTR, KC_TILD, _______,                            _______, KC_PGUP, KC_PGDN, _______, _______, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      _______, _______, KC_DLR,  KC_PERC, KC_CIRC, KC_MINS,                            KC_EQL,  KC_UP,   KC_LEFT, KC_RGHT, _______, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, _______, KC_EXLM, KC_AT,   KC_HASH, KC_UNDS, _SW_LANG,         _______, KC_PPLS, KC_DOWN, NAV,     _______, _______, _______,
+     _______, _______, KC_EXLM, KC_AT,   KC_HASH, KC_UNDS, SW_LANG,          _______, KC_PPLS, KC_DOWN, NAV,     _______, _______, _______,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
-                                    _______, _______, _SW_LANG,                  _______, _______, _______
+                                    _______, _______, SW_LANG,                  _______, _______, _______
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
@@ -88,9 +88,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      _______, _______, _______, _______, _______, _______,                            _______, _______, _______, _______, _______, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, _______, KC_WH_D, KC_MS_U, KC_WH_U, _______,                            _______, _______, _______, _______, _______, _______,
+     _______, _______, KC_WH_D, KC_MS_U, KC_WH_U, _______,                            _______, KC_P,    _______, _______, _______, _______,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     _______, KC_WH_L, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_R,                            _______, _______, _______, _______, _______, _______,
+     _______, KC_WH_L, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_R,                            _______, A_SHFT_L,LALT_L,  LALT_R,  A_SHFT_R,_______,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      _______, _______, KC_BTN2, KC_BTN3, KC_BTN1, _______, NAV,              _______, _______, _______, NAV,     _______, _______, _______,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
@@ -101,80 +101,72 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 };
 
 
-void handle_regular_layers(enum layers_keycodes layer, bool pressed) {
-    if (pressed) {
-        layer_on(layer);
-        update_tri_layer(_LOWER, _RAISE, _NAV);
-    } else {
-        layer_off(layer);
-        update_tri_layer(_LOWER, _RAISE, _NAV);
-    }
-}
-
-void handle_macros(uint16_t keycode, keyrecord_t *record) {
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     // https://docs.qmk.fm/#/feature_macros?id=using-macros-in-c-keymaps
     switch(keycode) {
-        case _SW_LANG:
+        case SW_LANG:
+            // register_code(KC_LGUI);
+            // register_code(KC_K);
+            // unregister_code(KC_K);
+            // unregister_code(KC_LGUI);
+
             register_code(KC_LGUI);
             register_code(KC_K);
-            unregister_code(KC_K);
-            unregister_code(KC_LGUI);
-            break;
+            return true;
 
-        case _SHFT_L:
-            SEND_STRING(SS_DOWN(X_LSHIFT) SS_TAP(X_LEFT) SS_UP(X_LSHIFT));
-            break;
+            // SEND_STRING(SS_DOWN(X_LGUI) SS_TAP(X_K) SS_UP(X_LGUI));
+            // TODO: if this doesn't work, try adding SS_DELAY()
+            return false;
 
-        case _SHFT_R:
-            SEND_STRING(SS_DOWN(X_LSHIFT) SS_TAP(X_RIGHT) SS_UP(X_LSHIFT));
-            break;
+        case LALT_L:
+            SEND_STRING(SS_DOWN(X_LALT) SS_TAP(X_LEFT) SS_UP(X_LALT));
+            return false;
 
-        case _A_SHFT_L:
+        case LALT_R:
+            SEND_STRING(SS_DOWN(X_LALT) SS_TAP(X_RIGHT) SS_UP(X_LALT));
+            return false;
+
+        case A_SHFT_L:
             SEND_STRING(SS_DOWN(X_LALT) SS_DOWN(X_LSHIFT) SS_TAP(X_LEFT) SS_UP(X_LSHIFT) SS_UP(X_LALT));
-            break;
+            return false;
 
-        case _A_SHFT_R:
+        case A_SHFT_R:
             SEND_STRING(SS_DOWN(X_LALT) SS_DOWN(X_LSHIFT) SS_TAP(X_RIGHT) SS_UP(X_LSHIFT) SS_UP(X_LALT));
-            break;
+            return false;
     }
-}
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  switch (keycode) {
-    case COLEM:
-      if (record->event.pressed) {
-        set_single_persistent_default_layer(_COLEMAK);
-      }
-      return false;
-
-    case QWERT:
-      if (record->event.pressed) {
-        set_single_persistent_default_layer(_QWERTY);
-      }
-      return false;
-
-    case LOWER:
-      handle_regular_layers(_LOWER, record->event.pressed);
-      return false;
-
-    case RAISE:
-      handle_regular_layers(_RAISE, record->event.pressed);
-      return false;
-
-    case NAV:
-      if (record->event.pressed) {
-        layer_on(_NAV);
-      } else {
-        layer_off(_NAV);
-      }
-      return false;
-
-    default:
-      handle_macros(keycode, record);
-      break;
-  }
 
   return true;
 }
 
+layer_state_t layer_state_set_user(layer_state_t state) {
+  return update_tri_layer_state(state, _LOWER, _RAISE, _NAV);
+}
+
 // TODO: consider layer-change code for per-layer rgb lighting: https://docs.qmk.fm/#/custom_quantum_functions?id=layer-change-code
+
+
+
+
+
+
+// TODO:
+/* Current status is:
+   1. SW_LANG macro doesn't work. Possibly all macros.
+      I added the missing macros to NAV layer for verification.
+
+   2. NAV layer doesn't work via raise layer key.
+
+   3. Need to further customize layer change:
+      I need to always be able to switch language via macro, and only do it via macro.
+      This way I can expand the macro to also switch between qwerty-colemak, so that I'm always in english-colemak and hebrew-qwerty.
+      This can be acheived by having some boolean state for english/hebrew, that is checked and updated on each SW_LANG execution,
+      along with actually changing between colemak and qwerty according to its state.
+      This would require another macro to only flip the boolean state, in case I get stuck in colemak-hebrew or qwerty-english.
+
+      Something like this:
+      struct state_t {
+          bool english;
+          bool colemak;
+      };
+      ?
+*/
--- keyboards/keebio/iris/keymaps/gelbermann-colemak/rules.mk
+++ keyboards/keebio/iris/keymaps/gelbermann-colemak/rules.mk
@@ -1,3 +1,2 @@
-VIA_ENABLE = yes
 MOUSEKEY_ENABLE = yes
 RGBLIGHT_ENABLE = no       # Disable WS2812 RGB underlight.
