230265d8c694ec5a03675797da366baa1bfe6987
Niv Gelbermann <niv.gelbermann@gmail.com>
2023-04-13 15:49:40 +0300

    implement default keymap

--- keyboards/handwired/gelbermann/numpad/keymaps/default/keymap.c
+++ keyboards/handwired/gelbermann/numpad/keymaps/default/keymap.c
@@ -3,19 +3,25 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
      * ┌───┬───┬───┬───┐
-     * │ 7 │ 8 │ 9 │ / │
+     * │ X │ X │ X │ X │
      * ├───┼───┼───┼───┤
-     * │ 4 │ 5 │ 6 │ * │
+     * │Num│ / │ * │ - │
      * ├───┼───┼───┼───┤
-     * │ 1 │ 2 │ 3 │ - │
+     * │ 7 │ 8 │ 9 │ + │
+     * ├───┼───┼───┼   │
+     * │ 4 │ 5 │ 6 │   │
      * ├───┼───┼───┼───┤
-     * │ 0 │ . │Ent│ + │
-     * └───┴───┴───┴───┘
+     * │ 1 │ 2 │ 3 │Ent│
+     * ├───────┼───┼   │
+     * │   0   │ . │   │
+     * └───────┴───┴───┘
      */
-    [0] = LAYOUT_ortho_4x4(
-        KC_P7,   KC_P8,   KC_P9,   KC_PSLS,
-        KC_P4,   KC_P5,   KC_P6,   KC_PAST,
-        KC_P1,   KC_P2,   KC_P3,   KC_PMNS,
-        KC_P0,   KC_PDOT, KC_PENT, KC_PPLS
+    [0] = LAYOUT_ortho_6x4(
+        _______,  _______,  _______,  _______,
+        KC_NUM,   KC_PSLS,  KC_PAST,  KC_PMNS,
+        KC_P7,    KC_P8,    KC_P9,    KC_PPLS,
+        KC_P4,    KC_P5,    KC_P6,
+        KC_P1,    KC_P2,    KC_P3,    KC_PENT,
+                  KC_P0,    KC_PDOT
     )
 };
